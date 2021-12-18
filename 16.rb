98e991fb65e6d6171f4776944e242e9b07f08b4d
Jeremy Stanley <jeremy@instructure.com>
2021-12-18 14:09:10 -0700

    okay, this isn't the same problem as I thought it was

--- 16.rb
+++ 16.rb
@@ -1,18 +1,28 @@
 dance = ARGF.gets.chomp.split(",")
 progs = ('a'..'p').to_a
 
-dance.each do |move|
-  case move[0]
-  when 's'
-    progs = progs.rotate(-move[1..].to_i)
-  when 'x'
-    a, b = move[1..].split("/").map(&:to_i)
-    progs[a], progs[b] = progs[b], progs[a]
-  when 'p'
-    p, q = move[1..].split("/")
-    a, b = progs.index(p), progs.index(q)
-    progs[a], progs[b] = progs[b], progs[a]
+hmap = {}
+(1..100).each do |i|
+  dance.each do |move|
+    case move[0]
+    when 's'
+      progs = progs.rotate(-move[1..].to_i)
+    when 'x'
+      a, b = move[1..].split("/").map(&:to_i)
+      progs[a], progs[b] = progs[b], progs[a]
+    when 'p'
+      p, q = move[1..].split("/")
+      a, b = progs.index(p), progs.index(q)
+      progs[a], progs[b] = progs[b], progs[a]
+    end
   end
+  perm = progs.join
+  if hmap.key?(perm)
+    puts "** previously found at #{hmap[perm]}"
+  else
+    hmap[perm] = i
+  end
+  puts "#{i}. #{perm}"
 end
 
-puts progs.join
+#puts ('a'..'p').map { |a| progs.index(a) }.inspect
